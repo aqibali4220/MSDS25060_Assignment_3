e65584e9ccd6b2a72e65e6a702cc1689ebdc0f7a
aqibali <aqibaliriaz@gmail.com>
2025-11-16 18:43:29 +0500

    commit solution for question no 3

--- solutions.R
+++ solutions.R
@@ -8,3 +8,13 @@ print(q1_result)
 q2_result <- film_dt[, .(avg_rental_rate = mean(rental_rate)), by = rating]
 print("Question 2: Average rental rate by rating")
 print(q2_result)
+
+# Question 3
+film_language <- merge(film_dt, language_dt, 
+                       by.x = "language_id", 
+                       by.y = "language_id")
+
+q3_result <- film_language[, .(film_count = .N), by = name]
+setnames(q3_result, "name", "language")
+print("Question 3: Total films by language")
+print(q3_result)
